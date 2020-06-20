76bef682f795139ec4970a1c9f570b9004f126a2
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2020-06-20 09:07:51 +0100

    Completed basic framework
    + Framework for index.js done
    Left a space for the Mod and Admin checks though
    
    + Added Ping command
    
    + Added a command file template for myself, in a gitignored folder

---
+++ commands/ping.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const { client } = require('../bot_modules/constants.js');
+
+let { PREFIX } = require('../config.js');
+
+
+module.exports = {
+    name: 'ping',
+    description: 'Pong! Returns your average ping to the Bot in milliseconds',
+    usage: ' ',
+    //aliases: [''],
+    //args: true,
+    commandType: 'general',
+    //cooldown: 3, // IN SECONDS
+
+    // LIMITATION MUST BE ONE OF THE FOLLOWING:
+    //    'dev' - Limits to me only, as the Bot's Developer
+    //    'owner' - Limits to Guild Owner and me only
+    //    'admin' - Those set as "Admin" in the Bot, the Guild Owner, and me only
+    //    'mod' - Those set as either "Mod" or "Admin", and the Guild Owner, and me only
+    //     otherwise, comment out for everyone to be able to use
+    //limitation: 'owner',
+
+    async execute(message) {
+      
+      return await message.reply(`Pong! \n Your ping is ${message.client.ws.ping.toFixed(2)}ms`);
+
+      //END OF COMMAND
+    },
+};
--- index.js
+++ index.js
@@ -108,6 +108,258 @@ client.once('ready', async () => {
 
 
 
+
+
+
+
+
+
+// ********** MESSAGE EVENT
+client.on('message', async (message) => {
+
+  // Prevent use in DMs
+  if ( message.channel.type === "dm" ) {
+    return;
+  }
+
+  // Prevent other Bots from triggering this Bot
+  if ( message.author.bot ) {
+    return;
+  }
+
+
+
+
+
+
+
+
+
+
+  // ***** Check for READ and SEND permissions
+  let botMember = message.guild.members.resolve(client.user.id); // Resolve GuildMember object from this Bot's User ID
+  let readPerms = botMember.hasPermission('VIEW_CHANNEL', {
+    checkAdmin: true
+  });
+  let sendPerms = botMember.hasPermission('SEND_MESSAGES', {
+    checkAdmin: true
+  });
+
+  // IF NO PERMISSION, ping a quick DM to Guild Owner
+  if ( !readPerms || !sendPerms ) {
+    let guildOwner = message.guild.owner;
+    let ownerDMs = await guildOwner.createDM();
+    return await ownerDMs.send(`Buzz! I don't seem to have the \`VIEW_CHANNELS\`, \`READ_MESSAGES\`, or \`SEND_MESSAGES\` permission(s) in **${message.guild.name}**!
+    I need these permissions to respond to my commands and be a useful Leveling Bot!`);
+  }
+
+
+
+
+
+
+
+
+
+
+  // ***** PREFIX CHECK
+  const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(PREFIX)})\\s*`);
+  
+
+
+
+
+
+
+
+
+
+  // ***** NO PREFIX
+  if ( !prefixRegex.test(message.content) ) {
+    return;
+  }
+  // ***** YES PREFIX
+  else {
+
+    // *** Fetch command
+
+    // Slide PREFIX off command
+    const [, matchedPrefix] = message.content.match(prefixRegex);
+    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+    // Slaps the cmd into its own var
+    const commandName = args.shift().toLowerCase();
+    // If there is NOT a command with the given name or aliases, exit early
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;
+
+
+
+
+    // *** Command Cooldowns
+    // If a command has 'cooldown: x,' it will enable cooldown IN SECONDS
+    if ( !cooldowns.has(command.name) ) {
+      cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    let cooldownAmount = (command.cooldown || 3) * 1000;
+
+
+    // OVERRIDE COOLDOWN IF THIS IS TRIGGERED
+    // A check for missing parameters
+    // If a cmd has 'args: true,', it will throw the error
+    // Requires the cmd file to have 'usage: '<user> <role>',' or similar
+    if (command.args && !args.length) {
+      let reply = `You didn't provide any arguments, ${message.author}!`;
+      if (command.usage) {
+        reply += `\nThe proper usage would be: \`${PREFIX}${command.name} ${command.usage}\``;
+      }
+
+      // Override larger cooldowns
+      if (timestamps.has(message.author.id) === false) {
+        cooldownAmount = 1000;
+      }
+
+
+      await message.channel.send(reply);
+    }
+
+
+
+    if ( timestamps.has(message.author.id) ) {
+      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        let timeLeft = (expirationTime - now) / 1000;
+
+        // If greater than 60 Seconds, convert into Minutes
+        if (timeLeft > 60 && timeLeft < 3600) {
+          timeLeft = timeLeft / 60;
+          return await message.reply(`Please wait ${timeLeft.toFixed(1)} more minute(s) before reusing the \`${command.name}\` command.`);
+        }
+        // If greater than 3600 Seconds, convert into Hours
+        else if (timeLeft > 3600) {
+          timeLeft = timeLeft / 3600;
+          return await message.reply(`Please wait ${timeLeft.toFixed(1)} more hour(s) before reusing the \`${command.name}\` command.`);
+        }
+
+        return await message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+      }
+    } else {
+      timestamps.set(message.author.id, now);
+      setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    }
+
+
+
+
+
+
+
+
+
+
+    // *** Extra Checks
+
+    // Check limitation
+    if ( command.limitation ) {
+
+      switch (command.limitation) {
+
+        // BOT DEV ONLY
+        case 'dev':
+          if ( message.author.id !== '156482326887530498' ) {
+            return await message.reply(`Sorry, but this command can only be used by the Bot's developer.`);
+          }
+          break;
+
+        // GUILD OWNER
+        case 'owner':
+          if ( message.author.id !== ( message.guild.ownerID || '156482326887530498' ) ) {
+            return await message.reply(`Sorry, but this command can only be used by the Owner of this Guild (**${message.guild.owner.displayName}**).`);
+          }
+          break;
+
+      }
+
+    }
+
+    // A check for missing parameters
+    // TO catch from above
+    if (command.args && !args.length) {
+      return;
+    }
+    
+
+
+
+
+
+
+
+
+
+    // *** Extra Permissions Check
+    let embedLinksPerm = botMember.hasPermission('EMBED_LINKS', {
+      checkAdmin: true
+    });
+    let attachFilesPerm = botMember.hasPermission('ATTACH_FILES', {
+      checkAdmin: true
+    });
+
+    // Embed Links Permission
+    if ( !embedLinksPerm && command.name !== 'ping' ) {
+      return await message.channel.send(`Sorry ${message.author}, but I seem to be missing the \`EMBED_LINKS\` permission.`);
+    }
+
+    // Attach Files Permission
+    if ( !attachFilesPerm && command.name === 'rank' ) {
+      return await message.channel.send(`Sorry ${message.author}, but I seem to be missing the \`ATTACH_FILES\` permission, which I need for this command!`);
+    }
+
+
+
+
+
+
+
+
+
+
+    // *** EXECUTE COMMAND
+    try {
+      command.execute(message, args);
+    } catch (error) {
+      console.error(error);
+      return await message.reply(`There was an error trying to run that command!`);
+    }
+
+  }
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ******************************
 // LOGIN TO BOT ACCOUNT
 client.login(TOKEN);
