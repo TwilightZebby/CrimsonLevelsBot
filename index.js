444df5c5260d17af2a80d0d9ed753d21ef261552
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2020-10-06 10:40:53 +0100

    re-add top.gg API stuff

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ let { PREFIX, TOKEN, DBLTOKEN } = require('./config.js');
 const { sequelize } = require('./bot_modules/constants.js');
 const Tables = require('./bot_modules/tables.js');
 
+// top.gg api stuff
+//const DBL = require("dblapi.js");
+//const dbl = new DBL(DBLTOKEN, client);
+
 // Maps / Collections
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
@@ -87,6 +91,23 @@ process.on('unhandledRejection', async (error) => {
   return await Errors.LogCustom(error, `Unhandled Promise Rejection:`)
 
 });
+
+
+// top.gg error handling
+/*dbl.on('error', async (e) => {
+
+  // Log to console
+  console.error(`DiscordBotList Error:\n ${e}`);
+
+  // Log to error log channel
+  let errorChannel = await client.guilds.fetch('681805468749922308');
+  errorChannel = errorChannel.channels.resolve('726336306497454081');
+
+  return await errorChannel.send(`\`\`\`DiscordBotList Error:\n
+  ${e}
+  \`\`\``);
+
+})*/
 
 
 // Discord Error Handling
